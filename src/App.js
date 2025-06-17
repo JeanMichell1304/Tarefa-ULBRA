8b7bfa6084f6dc401340b57b23f645fd9ad9e628
Jean Michell Ritter <michelld777@gmail.com>
2025-06-17 10:43:01 -0300

    Adicionando todo os componentes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MeuComponente from './MeuComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Componente Principal (App.js)</h1>
+      <MeuComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MeuComponente.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MeuComponente() {
+  return (
+    <div>
+      <h2>Componente Secundário</h2>
+      <p>Este é um parágrafo dentro do componente secundário.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MeuComponente;
